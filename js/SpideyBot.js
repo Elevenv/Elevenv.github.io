026fe492ef3a36482f129ff681be81960f43e464
Elevenv <vitthaljadhav1420@gmail.com>
2022-08-22 00:58:10 +0530

    updated SpideyBot

--- js/SpideyBot.js
+++ js/SpideyBot.js
@@ -8,12 +8,15 @@ function SendMsg(){
     req.setRequestHeader('Content-type','application/json')
     const params={
         username :'Spidey Bot',
-        content : 'Name : '+Name+'\nEmail : '+Email+'\nSubject : '+Subject+'\nMessage : '+Message
+        content : 
+            'Name : '+Name+
+            '\nEmail : '+Email+
+            '\nSubject : '+Subject+
+            '\nMessage : '+Message
     }
     req.send(JSON.stringify(params))
     document.getElementById('name').value = ''
     document.getElementById('email').value = ''
     document.getElementById('subject').value = ''
     document.getElementById('message').value = ''
-    console.log(Name)
     }
